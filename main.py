d1619826e178e983919a2cb178db46b20662f0b6
Shreejay Subedi <109503553+nemesis089@users.noreply.github.com>
2023-12-08 14:35:18 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, jsonify
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+
+model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M")
+tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M")
 
 app = Flask(__name__)
 
+@app.route('/api', methods = ['GET'])
+def returnascii():
+    d = {}
+    inputchr = str(request.args['query'])
+    translator = pipeline('translation', model=model, tokenizer=tokenizer, src_lang="bod_Tibt", tgt_lang='eng_Latn', max_length = 400)
+    answer = (translator(inputchr))
+    d['output'] = answer
+    return d
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+if __name__ =="__main__":
+    app.run()
